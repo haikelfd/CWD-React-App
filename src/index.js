f6da1cf13e70e1490a2f461806aa8653d074d3e9
Haikel Fadhloun <51424729+haikelfd@users.noreply.github.com>
2020-03-23 05:34:42 -0700

    Update 1.0
    Adding cards

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import Card from './Card';
+import Cardlist from './Cardlist';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import  { robots } from './Robots';
 
-ReactDOM.render(<Hello/>,document.getElementById('root')
-);
+ReactDOM.render(
+            
+,document.getElementById('root'));
 serviceWorker.unregister();
