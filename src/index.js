3b613e65f398134a0968746618d670821bb110e7
Haikel Fadhloun <51424729+haikelfd@users.noreply.github.com>
2020-03-23 19:10:18 -0700

    Update 2.0
    Adding 10 Cards that include :
    - Unique picture
    -Name
    -Email
    Adding Search Box:
    -Filter cards based on name

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
-import Cardlist from './Cardlist';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import  { robots } from './Robots';
 
-ReactDOM.render(
-            
-,document.getElementById('root'));
+ReactDOM.render(< App/>,document.getElementById('root'));
 serviceWorker.unregister();
