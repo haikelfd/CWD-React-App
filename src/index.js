b958edcdcc2ba40c83ddebf7ab2d45f085f82d88
Haikel Fadhloun <51424729+haikelfd@users.noreply.github.com>
2020-04-01 23:45:03 -0700

    Changes
    -Using to Redux Library
    -R-Connect
    -R-Middleware
    -R-Async Actions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import{ createStore, applyMiddleware} from 'redux';
+import{ createlogger} from 'react-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import{ searchRobots, requestRobots} from './reducers';
 import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const logger = createLogger();
+const rootReducer = combineReducers({ searchRobots, requestRobots})
+const store =
+createStore(searchRobots, applyMiddleware(thunkMiddleware, logger))
+
+ReactDOM.render(
+       <provider store={store}>
+           <app/>
+
+       </provider>, document.getElemenetById('root'));
 registerServiceWorker();
